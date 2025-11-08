f1160e14622a9167a9e95fdbe2e6111d45960ad9
zituryoku0 <tommykouki1031@icloud.com>
2025-11-08 19:57:48 +0900

    <add>:サンプルAPI機能の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect, useState } from 'react';
+import type { UserInfo } from './interface/userInfo';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch("http://localhost:8080/login", {
+          method: "GET",
+          credentials: "include", // ←重要
+          headers: { "Content-Type": "application/json" },
+        }).then((res) => res.json())
+          .then((data: UserInfo) => {
+            console.log(data);
+          });
+        console.log(response);
+      } catch (err: unknown) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError(String(err));
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []); // 空の依存配列は、コンポーネントマウント時に一度だけ実行されることを意味します
+
+  if (loading) {
+    return <p>読み込み中...</p>;
+  }
+
+  if (error) {
+    return <p>エラー: {error}</p>;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
